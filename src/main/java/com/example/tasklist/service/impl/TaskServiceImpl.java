0fad34a487a967bd8fcd760975195ab43a606ecd
gintariukai <gintariukai@hotmail.com>
2023-04-05 04:01:22 +0300

    Project structure 6: Description of service, create package dto and auth. Must implement the level service

--- src/main/java/com/example/tasklist/service/impl/TaskServiceImpl.java
+++ src/main/java/com/example/tasklist/service/impl/TaskServiceImpl.java
@@ -2,9 +2,11 @@ package com.example.tasklist.service.impl;
 
 import com.example.tasklist.domain.task.Task;
 import com.example.tasklist.service.TaskService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TaskServiceImpl implements TaskService {
 
     @Override
--- src/main/java/com/example/tasklist/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/tasklist/service/impl/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.tasklist.service.impl;
 
 import com.example.tasklist.domain.user.User;
 import com.example.tasklist.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Override
