410b90579aabd649f96c04fe5ab7a2b58240e01a
gintariukai <gintariukai@hotmail.com>
2023-04-05 03:30:31 +0300

    Project structure 5: Description of service, create package implement. Must implement the level service

---
+++ src/main/java/com/example/tasklist/service/impl/TaskServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.tasklist.service.impl;
+
+import com.example.tasklist.domain.task.Task;
+import com.example.tasklist.service.TaskService;
+
+import java.util.List;
+
+public class TaskServiceImpl implements TaskService {
+
+    @Override
+    public Task getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Task> getAllByUserId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Task update(Task task) {
+        return null;
+    }
+
+    @Override
+    public Task create(Task task) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
