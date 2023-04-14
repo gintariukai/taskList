65fe5aa8f845227c8a31c8c07038dee5dc2a0537
gintariukai <gintariukai@hotmail.com>
2023-04-14 13:11:31 +0300

    Implementing services and repositories with JDBC 18: Task service implementation

--- src/main/java/com/example/tasklist/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/tasklist/service/impl/UserServiceImpl.java
@@ -1,15 +1,24 @@
 package com.example.tasklist.service.impl;
 
+import com.example.tasklist.domain.exception.ResourceNotFoundException;
 import com.example.tasklist.domain.user.User;
+import com.example.tasklist.repository.UserRepository;
 import com.example.tasklist.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+
     @Override
+    @Transactional(readOnly = true)
     public User getById(Long id) {
-        return null;
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
