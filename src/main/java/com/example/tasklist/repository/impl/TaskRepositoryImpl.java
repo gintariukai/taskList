6b436181200ed2363e84963881543c83b7debe43
gintariukai <gintariukai@hotmail.com>
2023-04-17 11:25:31 +0300

    Implementing services and repositories with JDBC 20: TaskRepository implementation and create DataSourceConfig

--- src/main/java/com/example/tasklist/repository/impl/TaskRepositoryImpl.java
+++ src/main/java/com/example/tasklist/repository/impl/TaskRepositoryImpl.java
@@ -1,14 +1,22 @@
 package com.example.tasklist.repository.impl;
 
 import com.example.tasklist.domain.task.Task;
+import com.example.tasklist.repository.DataSourceConfig;
 import com.example.tasklist.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class TaskRepositoryImpl implements TaskRepository {
+
+    private final DataSourceConfig dataSourceConfig;
+
+    private final String FIND_BY_ID = """
+            """;
 
     @Override
     public Optional<Task> findById(Long id) {
