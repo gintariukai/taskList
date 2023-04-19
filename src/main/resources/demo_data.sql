07a36aed7e657ba5b60e1161c04b468812b549ec
gintariukai <gintariukai@hotmail.com>
2023-04-19 17:43:41 +0300

    25: Bug Fix, add id to users and tasks

--- src/main/resources/demo_data.sql
+++ src/main/resources/demo_data.sql
@@ -1,12 +1,12 @@
-insert into users (name, username, password)
-values ('John Doe', 'johndoe@gmail.com', '$2a$10$Xl0yhvzLIaJCDdKBS0Lld.ksK7c2Zytg/ZKFdtIYYQUv8rUfvCR4W'),
-       ('Mike Smith', 'mikesmith@yahoo.com', '$2a$10$fFLij9aYgaNCFPTL9WcA/uoCRukxnwf.vOQ8nrEEOskrCNmGsxY7m');
+insert into users (id, name, username, password)
+values ('1', 'John Doe', 'johndoe@gmail.com', '$2a$10$Xl0yhvzLIaJCDdKBS0Lld.ksK7c2Zytg/ZKFdtIYYQUv8rUfvCR4W'),
+       ('2', 'Mike Smith', 'mikesmith@yahoo.com', '$2a$10$fFLij9aYgaNCFPTL9WcA/uoCRukxnwf.vOQ8nrEEOskrCNmGsxY7m');
 
-insert into tasks (title, description, status, expiration_date)
-values ('Buy cheese', null, 'TODO', '2023-01-29 12:00:00'),
-       ('Do homework', 'Math, Physics, Literature', 'IN_PROGRESS', '2023-01-31 00:00:00'),
-       ('Clean rooms', null, 'DONE', null),
-       ('Call Mike', 'Ask about meeting', 'TODO', '2023-02-01 00:00:00');
+insert into tasks (id, title, description, status, expiration_date)
+values ('1', 'Buy cheese', null, 'TODO', '2023-01-29 12:00:00'),
+       ('2', 'Do homework', 'Math, Physics, Literature', 'IN_PROGRESS', '2023-01-31 00:00:00'),
+       ('3', 'Clean rooms', null, 'DONE', null),
+       ('4', 'Call Mike', 'Ask about meeting', 'TODO', '2023-02-01 00:00:00');
 
 insert into users_tasks (task_id, user_id)
 values (1, 2),
